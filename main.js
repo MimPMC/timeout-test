720589ff97b6c7901f21c8cedd42c982e9481d08
mims <113519976+MimPMC@users.noreply.github.com>
2022-11-15 09:41:59 +0100

    hej

---
+++ main.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', main);
+
+function main() {
+    //starta program
+}
+setTimeout(changeBackgroundColor, 4000);
+
+function changeBackgroundColor() {
+    document.body.style.background = "Lightblue"
+}

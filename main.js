09116dcc796bff997775866233ea137a1b99cb6f
mims <113519976+MimPMC@users.noreply.github.com>
2022-11-15 10:45:37 +0100

    fail timeout-interval

--- main.js
+++ main.js
@@ -1,10 +1,49 @@
 window.addEventListener('DOMContentLoaded', main);
-
+let timeOutRef;
 function main() {
     //starta program
+    addEventListeners();
+    timeOutRef = setTimeout(changeBackgroundColor, 4000);
 }
-setTimeout(changeBackgroundColor, 4000);
+
 
 function changeBackgroundColor() {
-    document.body.style.background = "Lightblue"
+    document.body.style.background = getRandomColor();
+;}
+
+function addEventListeners() {
+    const cancelTimeOutButton = document.getElementById("cancel");
+    cancelTimeOutButton.addEventListener('click', cancelBackgroundChange);
+
+    const startIntervalButton = document.getElementById('start-interval')
+    startIntervalButton.addEventListener('click', startBackgroundChangeInveral)
+}
+
+function cancelBackgroundChange() {
+    clearTimeout(timeOutRef);
+}
+
+function startBackgroundChangeInveral() {
+    setInterval(changeBackgroundColor, 1000);
+}
+
+function getRandomColor() {
+    const value= Math.random();
+
+    if (value > 0,2) {
+        return 'blue";
+    }
+    else if (value > 0.4) {
+        return'red';
+    }
+    else if (value > 0.6) {
+        return'yellow';
+    }
+    else if (value > 0.8) {
+        return'grey';
+    }
+    else {
+        return'pink;
+    }
+
 }
